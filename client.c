344c371c17fd52dcbdbeed2390437255ad2ba9d2
Huang Po-Hsuan <aben20807@gmail.com>
2017-11-02 23:28:01 +0800

    Let create socket be into functions

--- client.c
+++ client.c
@@ -2,44 +2,42 @@
 
 int main(int argc, char **argv)
 {
-	// write someting here...
-	//socket的建立
+	int sockfd = create_client();
+	connect_to_server(sockfd, "127.0.0.1", 59487);
+	char message[] = {"Hi there"};
+	char receive_message[100] = {};
+	send(sockfd, message, sizeof(message), 0);
+	recv(sockfd, receive_message, sizeof(receive_message), 0);
+	printf("%s", receive_message);
+	printf("close Socket\n");
+	close(sockfd);
+	return 0;
+}
+
+int create_client()
+{
 	int sockfd = 0;
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
 	if (sockfd == -1) {
 		printf("Fail to create a socket.");
 	} else {
 		printf("Client started...\n");
 	}
 	fflush(stdout);
-	//socket的連線
+	return sockfd;
+}
 
+void connect_to_server(const int sockfd, const char *server_addr,
+                       const int port)
+{
 	struct sockaddr_in info;
 	memset(&info, 0, sizeof(info));
 	info.sin_family = PF_INET;
-
-	//localhost test
-	info.sin_addr.s_addr = inet_addr("127.0.0.1");
-	info.sin_port = htons(59487);
-
+	info.sin_addr.s_addr = inet_addr(server_addr);
+	info.sin_port = htons(port);
 	int err = connect(sockfd, (struct sockaddr *)&info, sizeof(info));
 	if (err == -1) {
-		printf("Connection error");
+		printf("Connection error\n");
 		fflush(stdout);
 	}
-
-	//Send a message to server
-	char message[] = {"Hi there"};
-	char receive_message[100] = {};
-	int n = 5;
-	while (n--) {
-		send(sockfd, message, sizeof(message), 0);
-	}
-	recv(sockfd, receive_message, sizeof(receive_message), 0);
-
-	printf("%s", receive_message);
-	printf("close Socket\n");
-	close(sockfd);
-	return 0;
 }
--- client.h
+++ client.h
@@ -3,5 +3,8 @@
 
 #include "protocol.h"
 
+int create_client();
+void connect_to_server(const int sockfd, const char *server_addr,
+                       const int port);
 
 #endif
