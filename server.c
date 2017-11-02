7f90f3af90e5d8102add3bcbb30d10ec14cdccf3
Huang Po-Hsuan <aben20807@gmail.com>
2017-11-02 09:22:46 +0800

    Clean code

--- server.c
+++ server.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
 	FILE *fin;
-	for (int i = 3; i < 4; i++) {
+	for (int i = 0; i < 6; i++) {
 		pid_t pid = i;
 		printf("\npid: %d\n", pid);
 		if (!open_file(&fin, create_status_path(pid))) {
@@ -19,28 +19,6 @@ int main(int argc, char **argv)
 			free(tmp);
 		}
 	}
-	FILE *fin1;
-	for (int i = 5; i < 6; i++) {
-		pid_t pid = i;
-		printf("\npid: %d\n", pid);
-		if (!open_file(&fin1, create_status_path(pid))) {
-			continue;
-		}
-		map m = create_status_map(fin1);
-		element_ptr curr_ptr = m;
-		while (curr_ptr != NULL) {
-			printf("%s:\t",  curr_ptr -> key);
-			printf("%s\n",  curr_ptr -> value);
-			element_ptr tmp = curr_ptr;
-			curr_ptr = curr_ptr -> next;
-			free(tmp);
-		}
-	}
-
-	// pid_t pid = 1;
-	// open_file(&fin, create_status_path(pid));
-	// map m = create_status_map(fin, pid);
-	// printf("%s %s", m-> key, m -> value);
 	printf("\nexit\n");
 	return 0;
 }
@@ -70,7 +48,6 @@ map create_status_map(FILE *fin)
 	char line_buffer[BUFSIZ]; // BUFSIZ is defined in stdio.h
 	int line_number = 0;
 	while (fgets(line_buffer, sizeof(line_buffer), fin)) {
-		// while ((c = fgetc(fin)) != EOF) {
 		// printf("%4d: %s", line_number, line_buffer);
 		char *key, *value;
 		split_key_value(line_buffer, &key, &value);
@@ -83,62 +60,7 @@ map create_status_map(FILE *fin)
 		curr_ptr -> next = NULL;
 		memset(line_buffer, '\0', sizeof(line_buffer));
 		line_number++;
-	}/*
-		char c;
-		char *key, *value;
-		key = (char *) calloc(30, sizeof(char));
-		value = (char *) calloc(30, sizeof(char));
-		bool is_colon_appear = false;
-		int k_count = 0, v_count = 0;
-		while ((c = fgetc(fin)) != EOF) {
-	// printf("%4d: %s", line_number, line_buffer);
-	if (c == '\n') {
-	if (line_number != 0) {
-	MALLOC(curr_ptr -> next, sizeof(*curr_ptr));
-	curr_ptr = curr_ptr -> next;
 	}
-	// key[strlen(key)] = '\0';
-	for (int i = 0; i < strlen(value); i++) {
-	// (value)[i] = '\0';
-	printf("(%c)", (value)[i]);
-	}
-	// for (int i = strlen(value); i < 30; i++) {
-	// (value)[i] = '\0';
-	// printf("(%c)", (value)[i]);
-	// }
-	curr_ptr -> key = key;
-	curr_ptr -> value = value;
-	curr_ptr -> next = NULL;
-	is_colon_appear = false;
-	k_count = 0, v_count = 0;
-	key = (char *) calloc(30, sizeof(char));
-	value = (char *) calloc(30, sizeof(char));
-	for (int i = 0; i < strlen(value); i++) {
-	(value)[i] = '\0';
-	// printf("(%c)", (value)[i]);
-	}
-	printf("\n>%d\n", strlen(value));
-	line_number++;
-	} else {
-	if (c == ':' && !is_colon_appear) {
-	is_colon_appear = true;
-	} else if (!is_colon_appear) {
-	(key)[k_count++] = c;
-	} else {
-	if (c == ' ' || c == '\t' || \
-	c == 0x007f) { // ignore white space
-	continue;
-	}
-	// if (c <= 'z' && c >= 'a') { // ignore white space
-	// printf("%c\n", (*value)[count++]);
-	// continue;
-	(value)[v_count] = c;
-	// }
-	// printf("[%c]", (value)[v_count++]);
-	printf("l%d, %p\n", strlen(value), &(value[v_count++]));
-	}
-	}
-	}*/
 	fclose(fin);
 	return status_map;
 }
@@ -162,34 +84,7 @@ void split_key_value(char *line, char **key, char **value)
 				// ignore white space and tab
 				continue;
 			}
-			// if (line[i] <= 'z' && line[i] >= 'a') { // ignore white space
-			// printf("%c\n", (*value)[count++]);
-			// continue;
 			(*value)[count++] = line[i];
-			// }
-			// printf("%c\n", (*value)[count++]);
 		}
 	}
-	// y = (char *)malloc(sizeof(char)*300);
-	// for (int i = 0; i < strlen(line); i++){
-	//     (*key)[i] = line[i];
-	// }
-	//     (*key)[strlen(*key)] = '\0';
-	//     (*value)[strlen(*value)] = '\0';
-	//     for (int i = strlen(*key); i < 30; i++) {
-	//         (*key)[i] = '\0';
-	//         // printf("%c\n", (*value)[i]);
-	//     }
-	//     for (int i = 0; i < 30; i++) {
-	//         // (*key)[i] = '\0';
-	//         printf("%c", (*key)[i]);
-	//     }
-	//     for (int i = strlen(*value); i < 30; i++) {
-	//         (*value)[i] = '\0';
-	//         // printf("%c\n", (*value)[i]);
-	//     }
-	//     for (int i = 0; i < 30; i++) {
-	//         // (*value)[i] = '\0';
-	//         printf("%c", (*value)[i]);
-	//     }
 }
